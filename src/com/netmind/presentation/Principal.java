6d70bc81f3e6e1662b8231fa2818bf8555fa4091
armand.carreras <armand.fn.fal@gmail.com>
2021-02-16 21:01:21 +0100

    first commit

---
+++ src/com/netmind/presentation/Principal.java
@@ -0,0 +1,11 @@
+ package com.netmind.presentation;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is my first Java Project");
+	}
+
+}
